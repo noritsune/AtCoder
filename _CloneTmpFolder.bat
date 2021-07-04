1f0b891f343d94ea6813fab9b531ce1e50f3b875
noritsune <bill.gatess4037@gmail.com>
2021-07-05 00:33:00 +0900

    add: _tmpフォルダのコピーを作成するバッチ

---
+++ _CloneTmpFolder.bat
@@ -0,0 +1,31 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+rem コンテストフォルダ作成
+mkdir AtCoderBeginnerContestXXX
+cd AtCoderBeginnerContestXXX
+
+rem 問題名の定義
+set problemNames[0]=A
+set problemNames[1]=B
+set problemNames[2]=C
+set problemNames[3]=D
+
+rem 問題ごとに処理を繰り返す
+for /l %%i in (0, 1, 3) do (
+	rem 問題名のフォルダを作って移動
+	set currentDir=!problemNames[%%i]!
+	mkdir !currentDir!
+	cd !currentDir!
+
+	rem プロジェクト初期化
+	dotnet new console
+
+	rem util.csをコピー
+	copy ..\..\_tmp\util\util.cs Program.cs
+
+	cd ..
+)
+
+endlocal
