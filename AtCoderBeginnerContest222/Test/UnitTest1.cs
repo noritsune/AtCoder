0b807ca6d3326cf1fe530bc5aa7cc5781739c311
noritsune <bill.gatess4037@gmail.com>
2021-10-10 19:09:25 +0900

    add: ABC222

---
+++ AtCoderBeginnerContest222/Test/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using util;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            const string pathOffset = "../../../SampleInOut/1/";
+            TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+        }
+        
+        [TestMethod]
+        public void Test2()
+        {
+            const string pathOffset = "../../../SampleInOut/2/";
+            TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+        }
+        
+        [TestMethod]
+        public void Test3()
+        {
+            const string pathOffset = "../../../SampleInOut/3/";
+            TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+        }
+        
+        private static void TestInOut(string inputFileName, string outputFileName)
+        {
+            using var input = new StreamReader(inputFileName);
+            using var output = new StringWriter();
+            Console.SetOut(output);
+            Console.SetIn(input);
+
+            Util.Main();
+
+            Assert.AreEqual(File.ReadAllText(outputFileName), output.ToString());
+        }
+    }
+}
