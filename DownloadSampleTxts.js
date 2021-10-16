2c9f593eba1d2cd5f30208990f951067d04c862e
noritsune <bill.gatess4037@gmail.com>
2021-10-16 22:40:43 +0900

    add: ARC128

--- DownloadSampleTxts.js
+++ DownloadSampleTxts.js
@@ -1,13 +1,16 @@
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
 	const inSampleEle = document.getElementById(`pre-sample${i * 2}`);
 	const outSampleEle = document.getElementById(`pre-sample${i * 2 + 1}`);
 	if(inSampleEle == null) break;
 
-	downloadTxtFile(`${i}In.txt`, inSampleEle.textContent);
-	downloadTxtFile(`${i}Out.txt`, outSampleEle.textContent);
+	downloadTxtFile(`${i + 1}In.txt`, inSampleEle.textContent);
+	downloadTxtFile(`${i + 1}Out.txt`, outSampleEle.textContent);
 }
 
 function downloadTxtFile(filename, textContent) {
+	// テストに通る様に改行コードをWindows用にする
+	textContent.replaceAll("\n", "\r\n");
+
 	const blob = new Blob([textContent],{type:"text/plain"});
 	const link = document.createElement('a');
 	link.href = URL.createObjectURL(blob);
