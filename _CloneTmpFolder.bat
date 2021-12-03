798f6ad19305c0e994a5c96b4e6c72c48f6b5d69
noritsune <bill.gatess4037@gmail.com>
2021-12-03 23:00:12 +0900

    change: テンプレートフォルダコピーバッチでRider起動まで行い、slnファイルは読み込まない

--- _CloneTmpFolder.bat
+++ _CloneTmpFolder.bat
@@ -38,7 +38,11 @@ for /l %%i in (0, 1, 3) do (
 	dotnet sln add !problemName!Test
 )
 
-rem Rider起動とソリューション読み込みまで済ませる。しかしcmdを閉じるとRiderが閉じる
-rider AtCoder.sln
+@REM rem Rider起動とソリューション読み込みまで済ませる。しかしcmdを閉じるとRiderが閉じる
+@REM rider AtCoder.sln
+
+rem Rider起動までを行う。ソリューション読み込みは手動でやる
+cd D:\Program Files\JetBrains\JetBrains Rider 2021.1.3\bin
+rider64.exe
 
 endlocal
