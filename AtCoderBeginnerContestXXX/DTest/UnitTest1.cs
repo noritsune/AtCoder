340c2c15dee75b6f40996fa9e250327192a68fa5
noritsune <bill.gatess4037@gmail.com>
2021-12-03 22:53:48 +0900

    add: ABC230

---
+++ AtCoderBeginnerContestXXX/DTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtCoder;
+
+namespace Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			const string pathOffset = "../../../SampleInOut/1/";
+			TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+		}
+		
+		[TestMethod]
+		public void Test2()
+		{
+			const string pathOffset = "../../../SampleInOut/2/";
+			TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+		}
+		
+		[TestMethod]
+		public void Test3()
+		{
+			const string pathOffset = "../../../SampleInOut/3/";
+			TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+		}
+		
+		[TestMethod]
+		public void Test4()
+		{
+			const string pathOffset = "../../../SampleInOut/4/";
+			TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+		}
+		
+		[TestMethod]
+		public void Test5()
+		{
+			const string pathOffset = "../../../SampleInOut/5/";
+			TestInOut(pathOffset + "In.txt", pathOffset + "Out.txt");
+		}
+
+		private static void TestInOut(string inputFileName, string outputFileName)
+		{
+			if (!File.Exists(inputFileName)) return;
+			
+			using var input = new StreamReader(inputFileName);
+			using var output = new StringWriter();
+			Console.SetOut(output);
+			Console.SetIn(input);
+
+			SolveExecuter.Main();
+
+			Assert.AreEqual(File.ReadAllText(outputFileName), output.ToString());
+		}
+	}
+}
