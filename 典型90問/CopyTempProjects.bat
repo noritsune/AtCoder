e71e7619df7b0142d50e349dfd6a873a9527fe51
noritsune <bill.gatess4037@gmail.com>
2021-12-30 18:30:40 +0900

    典型90問を解き始め

---
+++ 典型90問/CopyTempProjects.bat
@@ -0,0 +1,18 @@
+﻿rem 解答用プロジェクトを作成
+mkdir %1
+cd %1
+dotnet new console
+copy ..\tmp\Program.cs
+cd ..
+dotnet sln add %1
+
+rem テスト用プロジェクトを作成
+mkdir %1Test
+cd %1Test
+dotnet new mstest
+dotnet add reference ../%1
+copy ..\tmpTest\UnitTest1.cs
+mkdir SampleInOut
+cd ..
+
+dotnet sln add %1Test
