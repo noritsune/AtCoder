9faf63f57b2fae4a8fc7a2ab6e64070d50f46c16
noritsune <bill.gatess4037@gmail.com>
2021-12-30 18:18:08 +0900

    テストケースDL用のjavascriptで余計なテストケースがDLされるバグを修正

--- DownloadSampleTxts.js
+++ DownloadSampleTxts.js
@@ -1,16 +1,18 @@
-for (let i = 0; i < 3; i++) {
-	const inSampleEle = document.getElementById(`pre-sample${i * 2}`);
-	const outSampleEle = document.getElementById(`pre-sample${i * 2 + 1}`);
-	if(inSampleEle == null) break;
+(function(document,s){
+	for (let i = 0; i < 5; i++) {
+		const inSampleEle  = document.querySelector(`#task-statement .lang-ja #pre-sample${i * 2}`);
+		const outSampleEle  = document.querySelector(`#task-statement .lang-ja #pre-sample${i * 2 + 1}`);
+		if(inSampleEle == null) break;
 
-	downloadTxtFile(`${i + 1}In.txt`, inSampleEle.textContent);
-	downloadTxtFile(`${i + 1}Out.txt`, outSampleEle.textContent);
-}
+		downloadTxtFile(`${i + 1}In.txt`, inSampleEle.textContent);
+		downloadTxtFile(`${i + 1}Out.txt`, outSampleEle.textContent);
+	}
 
-function downloadTxtFile(filename, textContent) {
-	const blob = new Blob([textContent],{type:"text/plain"});
-	const link = document.createElement('a');
-	link.href = URL.createObjectURL(blob);
-	link.download = filename;
-	link.click();
-}
+	function downloadTxtFile(filename, textContent) {
+		const blob = new Blob([textContent],{type:"text/plain"});
+		const link = document.createElement('a');
+		link.href = URL.createObjectURL(blob);
+		link.download = filename;
+		link.click();
+	}
+})(document);
