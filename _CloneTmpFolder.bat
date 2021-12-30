3f0822b44a11a0457fb71436a2538ab0ea7b908e
noritsune <bill.gatess4037@gmail.com>
2021-12-30 17:26:25 +0900

    テンプレートコピーバッチからRider起動処理を削除

--- _CloneTmpFolder.bat
+++ _CloneTmpFolder.bat
@@ -41,8 +41,8 @@ for /l %%i in (0, 1, 3) do (
 @REM rem Rider起動とソリューション読み込みまで済ませる。しかしcmdを閉じるとRiderが閉じる
 @REM rider AtCoder.sln
 
-rem Rider起動までを行う。ソリューション読み込みは手動でやる
-cd D:\Program Files\JetBrains\JetBrains Rider 2021.1.3\bin
-rider64.exe
+@REM rem Rider起動までを行う。ソリューション読み込みは手動でやる
+@REM cd D:\Program Files\JetBrains\JetBrains Rider 2021.1.3\bin
+@REM rider64.exe
 
 endlocal
