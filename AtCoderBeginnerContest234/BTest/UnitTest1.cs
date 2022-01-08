849b73b95730ce96f6f24d13aad1c7fcadd53e80
noritsune <bill.gatess4037@gmail.com>
2022-01-09 02:39:39 +0900

    ABC234を追加

---
+++ AtCoderBeginnerContest234/BTest/UnitTest1.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtCoder;
+
+namespace Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		const string testCaseDirPath = "../../../SampleInOut/";
+		
+		[TestMethod]
+		public void Test1()
+		{
+			TestInOut(testCaseDirPath + "1In.txt", testCaseDirPath + "1Out.txt");
+		}
+		
+		[TestMethod]
+		public void Test2()
+		{
+			TestInOut(testCaseDirPath + "2In.txt", testCaseDirPath + "2Out.txt");
+		}
+		
+		// [TestMethod]
+		// public void Test3()
+		// {
+		// 	TestInOut(testCaseDirPath + "3In.txt", testCaseDirPath + "3Out.txt");
+		// }
+		//
+		// [TestMethod]
+		// public void Test4()
+		// {
+		// 	TestInOut(testCaseDirPath + "4In.txt", testCaseDirPath + "4Out.txt");
+		// }
+		
+		private static void TestInOut(string inputFileName, string outputFileName)
+		{
+			// if (!File.Exists(inputFileName)) return;
+			
+			using var input = new StreamReader(inputFileName);
+			using var output = new StringWriter();
+			Console.SetOut(output);
+			Console.SetIn(input);
+
+			SolveExecuter.Main();
+
+			string expected = File.ReadAllText(outputFileName);
+			string actual = output.ToString();
+			
+			// 改行コードを揃える
+			expected = expected.Replace("\r\n", "\n");
+			actual = actual.Replace("\r\n", "\n");
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
