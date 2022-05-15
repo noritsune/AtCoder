590b161c50c7438946269f036c4ce4843d814900
noritsune <bill.gatess4037@gmail.com>
2022-05-15 17:33:18 +0900

    change: テンプレート作成バッチでRiderの起動をしない

--- _CloneTmpFolder.bat
+++ _CloneTmpFolder.bat
@@ -40,7 +40,7 @@ for /l %%i in (0, 1, 4) do (
 )
 
 @REM rem Rider起動とソリューション読み込みまで済ませる。しかしcmdを閉じるとRiderが閉じる
-rider KyoPro.sln
+@REM rider KyoPro.sln
 
 @REM rem Rider起動までを行う。ソリューション読み込みは手動でやる
 @REM cd D:\Program Files\JetBrains\JetBrains Rider 2021.1.3\bin
