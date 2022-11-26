5a5fc3fe69de502582783db6eb92cb229d0ba75a
noritsune <bill.gatess4037@gmail.com>
2022-11-26 22:28:11 +0900

    add: テンプレートコードにdecimalの平方根を求める関数を追加

--- _tmp/KyoPro/Program.cs
+++ _tmp/KyoPro/Program.cs
@@ -200,6 +200,24 @@ namespace KyoPro {
             }
             return min;
         }
+
+        /// <summary>
+        /// doubleより高精度な平方根を求める
+        /// </summary>
+        public static decimal Sqrt(decimal x, decimal epsilon = 0.0M)
+        {
+            if (x < 0) throw new OverflowException("Cannot calculate square root from a negative number");
+
+            decimal current = (decimal)Math.Sqrt((double)x), previous;
+            do
+            {
+                previous = current;
+                if (previous == 0.0M) return 0;
+                current = (previous + x / previous) / 2;
+            }
+            while (Math.Abs(previous - current) > epsilon);
+            return current;
+        }
     }
 
     public static class Combination {
