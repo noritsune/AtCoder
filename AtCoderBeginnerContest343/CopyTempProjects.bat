c52ceecc63324c3962de03be1e1364be7d29a853
noritsune <bill.gatess4037@gmail.com>
2024-03-03 11:58:17 +0900

    add: ABC343

---
+++ AtCoderBeginnerContest343/CopyTempProjects.bat
@@ -0,0 +1,18 @@
+﻿rem 解答用プロジェクトを作成
+mkdir %1
+cd %1
+dotnet new console
+copy ..\E\Program.cs
+cd ..
+dotnet sln add %1
+
+rem テスト用プロジェクトを作成
+mkdir %1Test
+cd %1Test
+dotnet new mstest
+dotnet add reference ../%1
+copy ..\ETest\UnitTest1.cs
+mkdir SampleInOut
+cd ..
+
+dotnet sln add %1Test
