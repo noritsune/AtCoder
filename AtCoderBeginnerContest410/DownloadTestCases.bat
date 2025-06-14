a999d52f66ef224850e446f7aa599e4afd7cc87f
noritsune <bill.gatess4037@gmail.com>
2025-06-14 23:06:36 +0900

    ABC410に参加した

---
+++ AtCoderBeginnerContest410/DownloadTestCases.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set "contest=%~1"
+if "%contest%"=="" (
+    echo You need to specify the contest number. e.g. 021
+    exit /b 1
+)
+
+for %%c in (A B C D E F) do (
+    pushd "%%cTest"
+    oj d -f "%%%%i%%%%e.txt" "https://atcoder.jp/contests/abc%contest%/tasks/abc%contest%_%%c"
+    popd
+)
+
+echo All processes are done.
+endlocal
